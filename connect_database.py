631c266978074ad18517aa001c44a5c2f789fcac
HrishikV <65384819+HrishikV@users.noreply.github.com>
2021-10-20 17:45:04 +0530

    second commit

--- connect_database.py
+++ connect_database.py
@@ -12,5 +12,4 @@ client_secret='Rnn,eM323,ZSpZHGgXGPLLGddMxslotgkgKNmrs5.-YvSyB8SwmrIQAPsb-cIL1y6
 auth_provider=PlainTextAuthProvider(client_id,client_secret)
 cluster=Cluster(cloud=cloud_config,auth_provider=auth_provider)
 session=cluster.connect()
-df=pd.DataFrame(list(session.execute("select * from ineuron_incomeprediction.adult")))
-print(df.head())
+df=pd.DataFrame(list(session.execute("select * from income_prediction.adult;")))
---
+++ featur_selection.py
@@ -0,0 +1,1 @@
+from feature_enginering import df,workclass,race,country,occupation
